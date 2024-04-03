69856211a3ebd68f4eefe4a60a290b0e2ebae4bc
andy941 <finocchio.andrea94@gmail.com>
2024-04-03 22:30:12 +0100

    Chapter 15 - smart pointers

---
+++ 15_smart_pointers/01-02_boxes/src/main.rs
@@ -0,0 +1,44 @@
+use std::ops::Deref;
+
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
