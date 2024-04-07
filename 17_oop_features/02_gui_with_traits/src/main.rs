efef111e4aa05d421ed52545a7661acc7acaf950
andy941 <finocchio.andrea94@gmail.com>
2024-04-07 17:44:07 +0100

    Chapter 17 - OOP and Rust features

---
+++ 17_oop_features/02_gui_with_traits/src/main.rs
@@ -0,0 +1,24 @@
+use gui_with_traits::{Button, Screen, SelectBox};
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
---
+++ 17_oop_features/03_state_pattern/src/main.rs
@@ -0,0 +1,26 @@
+use state_pattern::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    // Better design using Rust type system and borrow-checker
+    // Have types for each type of post and transform them when needed
+     let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
